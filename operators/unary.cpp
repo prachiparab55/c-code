c10224d15249e6ff3dc2ed8c64bd7a43529e5c3b
PRACHI PARAB <prachi.parab.dev4@gmail.com>
2026-03-04 16:57:49 +0530

    Create unary.cpp

---
+++ operators/unary.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int y = 5;
+
+    cout << "Pre-increment (++y): " << ++y << endl;   
+    cout << "Post-increment (y++): " << y++ << endl; 
+    cout << "Pre-decrement (--y): " << --y << endl;  
+    cout << "Post-decrement (y--): " << y-- << endl;  
+
+    return 0;
+}
