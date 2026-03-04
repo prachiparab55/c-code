061056bd670f7b6adfcc1bd78187b4e648e59e6d
PRACHI PARAB <prachi.parab.dev4@gmail.com>
2026-03-04 16:31:02 +0530

    Create assignment.cpp

---
+++ operators/assignment.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 5;
+
+cout << (a += 3)<< endl;   
+cout << (a -= 2) << endl;   
+cout << ((a *= 4)) << endl;   
+cout << (a /= 2) << endl;      
+cout << (a %= 3) << endl;  
+
+    return 0;
+}
