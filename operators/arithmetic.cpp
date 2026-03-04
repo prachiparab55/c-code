d299db5922536a7e399a5d5db1cf637e76c21883
PRACHI PARAB <prachi.parab.dev4@gmail.com>
2026-03-04 15:46:05 +0530

    Create arithmetic.cpp

---
+++ operators/arithmetic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 15, b = 10;
+
+    cout << "Addition: " <<a + b << endl;
+    cout << "Subtraction: " <<a - b << endl;
+    cout << "Mul: " <<a * b << endl;
+    cout << "division: " <<a / b << endl;
+    cout << "Mod: " <<a % b << endl;
+
+    return 0;
+}
